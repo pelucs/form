de8219e21f990a176b91c1c69d0cd5d7944bf3dd
Pedro Lucas <88797361+pelucs@users.noreply.github.com>
2022-04-27 20:47:44 -0300

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,10 +1,10 @@
 import firebase from 'firebase/app';
 
 firebase.initializeApp(firebaseConfig = {
-  apiKey: "AIzaSyDm3RVD7xEL8cwu59ObJz81IGdh6et5-cU",
-  authDomain: "form-cadas.firebaseapp.com",
-  projectId: "form-cadas",
-  storageBucket: "form-cadas.appspot.com",
-  messagingSenderId: "514132523990",
-  appId: "1:514132523990:web:6fdbbaaf898c23921cc277"
+  apiKey: "SUA CHAVE",
+  authDomain: "SUA CHAVE",
+  projectId: "SUA CHAVE",
+  storageBucket: "SUA CHAVE",
+  messagingSenderId: "SUA CHAVE",
+  appId: "SUA CHAVE"
 });
